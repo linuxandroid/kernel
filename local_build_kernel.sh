dadd936951b7d5c453804413bf523e6abcb2bb81
steven <steven@geniatech.com>
2015-01-28 13:28:31 +0800

    MAKE: rm useless make files

--- local_build_kernel.sh
+++
@@ -1,7 +0,0 @@
-##!/bin/bash
-
-export ARCH=arm
-export CROSS_COMPILE=/home/yyoung.kim/Toolchain/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin/arm-eabi-
-
-make apq8084_sec_defconfig VARIANT_DEFCONFIG=apq8084_sec_lentislte_ktt_defconfig SELINUX_DEFCONFIG=selinux_defconfig TIMA_DEFCONFIG=tima_defconfig
-make
